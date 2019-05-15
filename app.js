890e47795b1ec69ab8f68740b698bdf168f005a1
Sagar <sagar.kumar@ebizontek.com>
2019-05-15 17:08:40 +0530

    Example of a very first event.

---
+++ app.js
@@ -0,0 +1,10 @@
+
+var events = require('events');
+
+var myEmitter = new events.EventEmitter();
+
+myEmitter.on('someEvent', function(message) {
+    console.log(message);
+});
+
+myEmitter.emit('someEvent', 'the event was emitted');
