5c19d84dc40510c1c539fe8e9de9a8b1ac359839
Sagar <sagar.kumar@ebizontek.com>
2019-05-16 17:37:07 +0530

    Added the file for reading the stream (file stream)

---
+++ stream.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname+'/input.txt', 'utf8');
+
+/**** Here the file is read into the chunk rather than the whole block. It does not load the whole data into the memory and then
+ * transfer it rather it only transfers a chunk of data and transfers it ****/
+
+myReadStream.on('data', function(chunk) {
+    console.log('New Chunk Received:');
+    console.log(chunk);
+});
